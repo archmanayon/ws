bcc2f8dcab1d0d6423373f40d4c3e40f79a2a5b8
arielman <archmanayon@gmail.com>
2023-05-23 19:29:41 +0800

    2 column div

--- resources/views/shcp_.blade.php
+++ resources/views/shcp_.blade.php
@@ -1,16 +1,17 @@
 @php
     use Illuminate\Support\Str;
-    use \Carbon\Carbon; 
+    use \Carbon\Carbon;
 @endphp
 <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         {{ 'Dashboard' }}
     </h2>
 </x-slot>
-
+<div class="lg:grid lg:grid-cols-2 w-full">
 <x-guest-layout>
+
     <div class="py-6">
-        <div class="mx-6 max-w-xl mx-auto sm:px-6 lg:px-8 text-center">
+        <div class="mx-6 max-w-xl sm:px-6 lg:px-8 text-center">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg px-6 ">
 
                 <div class="px-1 text-gray-900 dark:text-gray-100">
@@ -23,17 +24,17 @@
                         </td>
                         <td>
                             {{$date_->format('F j, Y')  }}
-                            
+
                         </td>
-                       
+
                 </div>
             </div>
-            
-        </div>  
-       
+
+        </div>
+
     </div>
 
-    
+
     <!-- Session Status -->
     <x-auth-session-status class= "m-auto-3" :status="session('status')" />
 
@@ -43,9 +44,9 @@
         <!-- USC ID Number -->
         <div>
             <x-input-label for="student_id" :value="__('USC ID Number')" />
-            <x-text-input id="student_id" class="block mt-1 w-full" 
-                            type="text" name="student_id" 
-                            :value="old('student_id')" 
+            <x-text-input id="student_id" class="block mt-1 w-full"
+                            type="text" name="student_id"
+                            :value="old('student_id')"
                             required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('student_id')" class="mt-2" />
         </div>
@@ -61,53 +62,94 @@
 
             <x-input-error :messages="$errors->get('punch_pw')" class="mt-2" />
         </div>
-      
+
 
         <div class="">
-            
+
             <x-primary-button class="m-auto-3 py-1 px-6 mt-4 mx-auto w-auto">
                 {{-- {{ __('Punch with your USC ID') }} --}}
-                {{ __( $in_out ? ($in_out == 'I'? 'Punch In with your USC ID' : 
+                {{ __( $in_out ? ($in_out == 'I'? 'Punch In with your USC ID' :
                     'Punch Out with your USC ID') :
                     'Punch In with your USC ID') }}
             </x-primary-button>
 
         </div>
     </form>
-      
+
     <!-- punches -->
     <div class="py-6">
-                                
+
         <table class=" text-center max-w-xl mx-auto sm:px-6 lg:px-8 p-6 text-gray-900 dark:text-gray-100" >
                 @if ($punches_today)
-                    
-               
+
+
                     @foreach ( $punches_today as $shcp)
                             <tr>
-                            
-                            <td> 
-                                {{ 
+
+                            <td>
+                                {{
                                     Carbon::createFromFormat('Hi', $shcp->hour??false)
                                     ->format('h:i a')
                                 }}
                             </td>
-                        
+
                             <td class="px-3">
                                 {{ '|' }}
                             </td>
-                            
-                            <td class=""> 
-                                {{ 
+
+                            <td class="">
+                                {{
                                     $shcp->in_out == 'I'? 'In' : 'Out'
                                 }}
                             </td>
-                            
+
                         </tr>
                     @endforeach
-                     
+
              @endif
-           
+
 
     </div>
 
 </x-guest-layout>
+
+<x-guest-layout>
+
+
+    <!-- punches -->
+    <div class="py-6">
+
+        <table class=" text-center max-w-xl mx-auto sm:px-6 lg:px-8 p-6 text-gray-900 dark:text-gray-100" >
+                @if ($punches_today)
+
+
+                    @foreach ( $punches_today as $shcp)
+                            <tr>
+
+                            <td>
+                                {{
+                                    Carbon::createFromFormat('Hi', $shcp->hour??false)
+                                    ->format('h:i a')
+                                }}
+                            </td>
+
+                            <td class="px-3">
+                                {{ '|' }}
+                            </td>
+
+                            <td class="">
+                                {{
+                                    $shcp->in_out == 'I'? 'In' : 'Out'
+                                }}
+                            </td>
+
+                        </tr>
+                    @endforeach
+
+             @endif
+
+
+    </div>
+
+</x-guest-layout>
+</div>
