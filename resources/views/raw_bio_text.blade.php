078bd346356e2d1ec6796de73279be56850efc54
arielman <archmanayon@gmail.com>
2023-07-10 20:08:01 +0800

    pending. validation for request punch source, displaying updated punch count side by side w/ raw

--- resources/views/raw_bio_text.blade.php
+++ resources/views/raw_bio_text.blade.php
@@ -76,22 +76,39 @@
                                         @if ($daily && $daily->punch->count()>=5)
 
                                             <tr class="bg-gray-700 border-b border-gray-600">
-                                                <td class="px-4 py-3">
-                                                    <a href="rawbio/{{$daily->user->timecard.$daily->punch[0]->date}}" class="font-semibold text-gray-600
-                                                        hover:text-gray-900 dark:text-gray-400 dark:hover:text-white
-                                                        focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                                                        {{ $daily->user->name." = ".$daily->punch->count()." | ".
-                                                            $daily->user->timecard.$daily->punch[0]->date}}
-                                                    </a>
 
-                                                </td>
+                                                @if ($daily->updated_bio[0]->date??false )
+
+                                                    <td class="px-4 py-3">
+
+                                                        {{ $daily->user->name." = ".$daily->punch->count()." | ".
+                                                            $daily->user->date." = ".$$daily->punch[0]->hour}}
+
+                                                    </td>
+
+                                                    <td class="px-4 py-3">
+                                                        {{ $daily->updated_bio[0]->name." = ".$daily->updated_bio->count()." | ".
+                                                            $daily->updated_bio[0]->date." = ".$$daily->updated_bio[0]->hour}}
+                                                    </td
+                                                @else
+
+                                                    <td class="px-4 py-3">
+
+                                                        <a href="rawbio/{{$daily->user->timecard.$daily->punch[0]->date}}" class="font-semibold text-gray-600
+                                                            hover:text-gray-900 dark:text-gray-400 dark:hover:text-white
+                                                            focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                                                            {{ $daily->user->name." = ".$daily->punch->count()." | ".
+                                                                $daily->user->timecard.$daily->punch[0]->date}}
+                                                        </a>
+
+                                                @endif
+
                                                 @if ($daily->updated_bio??false)
                                                     <td class="px-4 py-3">
                                                         {{ $daily->updated_bio[0]->date??false }}
                                                     </td>
                                                 @endif
-                                                
-                                                
+
                                             </tr>
 
                                             {{-- @foreach ($daily->punch as $punch)
@@ -120,38 +137,7 @@
 
                     {{-- 2nd column --}}
                     <div class="p-6 text-gray-900 dark:text-gray-100 border">
-                        <table class="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-800 text-gray-200">
-                            <tr class="text-left border-b border-gray-300">
-                                <th class="px-4 py-3">textfile
-                                </th>
 
-                            </tr>
-
-                            @foreach ( $mappedUser as $each_user => $daily)
-
-                                @foreach ( $daily as $daily)
-
-                                    @if ($daily && $daily->punch->count()>=5)
-
-                                        <tr class="bg-gray-700 border-b border-gray-600">
-                                            @if ($daily->updated_bio)
-
-                                                <td>
-                                                    {{ $daily->updated_bio[0] }}
-                                                </td>  
-                                                
-                                            @endif
-                                                                             
-                                            
-                                        </tr>                                       
-
-                                    @endif
-
-                                @endforeach
-
-                            @endforeach
-
-                        </table>
                     </div>
                 </div>
 
