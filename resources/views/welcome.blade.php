08df875b827c4453e446172ba2f80dfa87299f81
Archie <archmanayon@gmail.com>
2023-08-08 07:10:46 +0800

    removed icon from 'login'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,11 +57,11 @@
                                     @auth
                                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                                     @else
-                                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                                            <div class="bg-red-50 dark:bg-red-800/20 flex h-16 items-center justify-center mx-6 px-6 rounded-full">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">
+                                        <a href="{{ route('login') }}" class="style-azo-sans-regular font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                                            <div class="b dark:bg-gray-800/50 dark:ring-1 dark:ring-white/10 flex h-16 items-center justify-center mx-6 px-6 rounded-full rounded-lg">
+                                                {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-                                                </svg>
+                                                </svg> --}}
                                                 Log in
                                             </div>
                                             
