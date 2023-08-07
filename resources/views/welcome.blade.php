2093be3799c6bd822fe5d9f8845e976c04a18018
Archie <archmanayon@gmail.com>
2023-08-07 09:37:07 +0800

    deleted display or auth im path as pw

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
                                 
                                 @if (Route::has('login'))
                                     
-                                    {{ auth()->user()->image_path??false }} 
+                                    {{-- {{ auth()->user()->image_path??false }}  --}}
                         
                                     @auth
                                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
