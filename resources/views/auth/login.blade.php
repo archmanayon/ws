6642a2cbc1df9c5a811f17bb95dcd117856a8445
Archie <archmanayon@gmail.com>
2023-08-25 17:00:48 +0800

    some css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
     <div class="flex justify-end">
         <span class="py-4 text-white"> {{ 'or' }} <br></span>
         <x-link-button href="auth/google/redirect" 
-            class="px-1 py-1 active:bg-gray-900 bg-gray-800 border border-transparent dark:active:bg-gray-300 dark:bg-gray-200 dark:bg-gray-800 dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:hover:bg-white dark:text-gray-800 duration-150 ease-in-out flex focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 font-semibold hover:bg-gray-700 inline-flex items-center justify-center ml-0 p-0 rounded-md text-white text-xs tracking-widest transition uppercase w-48">
+            class="px-1 py-1 dark:bg-gray-800 bg-white w-48">
             {{-- <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
                 <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
             </svg>
