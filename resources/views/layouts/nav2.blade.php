352cf9910003bdc3ec42e608e9212fdabbe91d45
Archie <archmanayon@gmail.com>
2023-08-29 15:32:35 +0800

    css on schp display to punch

--- resources/views/layouts/nav2.blade.php
+++ resources/views/layouts/nav2.blade.php
@@ -1,7 +1,7 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-900 border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-gray-400 dark:bg-gray-900 border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    <div class="lg:px-8 mx-6 sm:px-6">
-        <div class="flex justify-between h-16">
+    <div class="lg:px-8 mx-6 sm:px-6 p-2 bg-gray-400 dark:bg-gray-900">
+        <div class="flex justify-between h-16 ">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
--- resources/views/shcp.blade.php
+++ resources/views/shcp.blade.php
@@ -18,18 +18,18 @@
             <!---------------first column------------------>
             <div class="py-6">
                 <div class="lg:px-8 mx-6 sm:px-6 text-center">
-                    <div class "bg-white dark:bg-gray-800 mt-6 overflow-hidden px-6 py-4 shadow-md sm:rounded-lg w-full">
-                        <div class="px-1 text-gray-900 dark:text-gray-100">
+                    <div class ="bg-gray-800 text-white dark:bg-gray-800 mt-6 overflow-hidden px-6 py-4 shadow-md sm:rounded-lg w-full">
+                        <div class="px-1 ">
                             <td>
 
                                     <div id="m_clock"
-                                    class="text-5xl mt-2 order-gray-300 dark:border-gray-700 dark:bg-gray-700 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600
+                                    class="text-5xl mt-2 order-gray-300 dark:border-gray-700 dark:bg-gray-700 dark:text-white focus:border-indigo-500 dark:focus:border-indigo-600
                                         focus:ring-indigo-500 dark:focus:ring-indigo-600 shadow-sm"
                                         >
 
                                     </div>
                                     <div id="sec"
-                                    class="dark:bg-gray-700 dark:border-gray-700 dark:focus:border-indigo-600 dark:focus:ring-indigo-600 dark:text-gray-300 focus:border-indigo-500 focus:ring-indigo-500 mt-0 order-gray-300 shadow-sm text-xl"
+                                    class="dark:bg-gray-700 dark:border-gray-700 dark:focus:border-indigo-600 dark:focus:ring-indigo-600 dark:text-white focus:border-indigo-500 focus:ring-indigo-500 mt-0 order-gray-300 shadow-sm text-xl"
                                         >
                                     clock
                                     </div>
@@ -42,8 +42,8 @@
 
                                 <!-- USC ID Number -->
                                 <div>
-                                    <x-input-label for="student_id" :value="__('USC ID Number')" class="block dark:text-gray-300 font-medium mt-8 text-center text-gray-700 text-sm" />
-                                    <x-text-input id="student_id" class="block mt-1 w-48 mx-auto"
+                                    <x-input-label for="student_id" :value="__('USC ID Number')" class="block text-white dark:text-white font-medium mt-8 text-center text-sm" />
+                                    <x-text-input id="student_id" class="block mt-1 w-48 mx-auto text-gray-900 dark:text-white"
                                                     type="text" name="student_id"
                                                     :value="old('student_id')"
                                                     :placeholder="$employee->student_id??false"
@@ -53,9 +53,9 @@
 
                                 <!-- Password -->
                                 <div class="mt-4">
-                                    <x-input-label for="punch_pw" :value="__('Password')" class="text-center"/>
+                                    <x-input-label for="punch_pw" :value="__('Password')" class="text-white dark:text-white text-center"/>
 
-                                    <x-text-input id="punch_pw" class="block mt-1 w-48 mx-auto"
+                                    <x-text-input id="punch_pw" class="block mt-1 w-48 mx-auto text-gray-900 dark:text-white"
                                                     type="password"
                                                     name="punch_pw"
                                                     required autocomplete="current-punch_pw" />
@@ -93,8 +93,8 @@
             <div class="py-6">
                 <div class="">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg px-6 ">
-                        <div class="px-1 text-gray-900 dark:text-gray-100">
-                            <table class="block dark:text-gray-100 p-6 py-1 sm:px-6 text-gray-900 w-auto" >
+                        <div class="px-1 text-gray-900 dark:text-white">
+                            <table class="block dark:text-white p-6 py-1 sm:px-6 text-gray-900 w-auto" >
                                 @if ($punches_today)
 
                                     <th>
@@ -135,7 +135,7 @@
             <div class="py-6">
                 <div class="">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg px-6 ">
-                        <div class="px-1 text-gray-900 dark:text-gray-100">
+                        <div class="px-1 text-gray-900 dark:text-white">
                             @if ($employee->username??false)
                                 <img src="images/{{$employee->username}}.jpg" alt="png" width="400" height="400">
                             @endif
