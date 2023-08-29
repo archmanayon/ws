a98d23f373507bf5a713b4d54c10a9909689d328
Archie <archmanayon@gmail.com>
2023-08-29 12:24:05 +0800

    controller. rawbio:'my_dtr': filters input dates. only view the dates set ty admin

--- app/Http/Controllers/RawbioController.php
+++ app/Http/Controllers/RawbioController.php
@@ -79,8 +79,12 @@ class RawbioController extends Controller
 
         $searched_user = auth()->user()??false;
 
-        $start_date = request('start_date') ?? 0;
-        $end_date = request('end_date') ?? 0;
+         $start_date = request('start_date')?? request('start_date') < $payroll_start->format('Y-m-d') ?
+                $payroll_start->format('Y-m-d') : request('start_date') ?? 0;  
+                        
+        $end_date = request('end_date') ?? request('end_date') > $payroll_end->format('Y-m-d') ?
+                $payroll_end->format('Y-m-d') : request('end_date') ?? 0;  
+                
         $period = CarbonPeriod::create($start_date, $end_date);
         $dates = $period->toArray();
         $collection_of_dates = collect($dates);
