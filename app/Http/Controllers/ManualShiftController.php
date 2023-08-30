d802c7cb2db0c71e1d6e4b9d328405a445944960
Archie Manayon Home <archmanayon@gmail.com>
2023-08-30 19:34:58 +0800

    used 'foreach' in making array for validating manual shift for selected days

--- app/Http/Controllers/ManualShiftController.php
+++ app/Http/Controllers/ManualShiftController.php
@@ -16,92 +16,109 @@ class ManualShiftController extends Controller
     public function official_($searched_user, $day, $date)
     {
          // if( $searched_user->manual_shifts->pluck('date')->contains( $date->format('Y-m-d')))
-                // {                             
+                // {
                 //     $Schedule_id =  $searched_user->manual_shifts->where('date',$date->format('Y-m-d'))
-                //                     ->pluck('schedule_id')->implode(', ');                                                          
+                //                     ->pluck('schedule_id')->implode(', ');
                 //     $official_am_in = $searched_user->schedule->find($Schedule_id)->Manual_in;
                 //     $official_am_out = $searched_user->schedule->find($Schedule_id)->Manual_out;
-                //     $official_am_num_hr = round((strtotime($official_am_out) - 
-                //         strtotime($official_am_in))/3600,2); 
+                //     $official_am_num_hr = round((strtotime($official_am_out) -
+                //         strtotime($official_am_in))/3600,2);
 
                 //     $am_late = round((strtotime($punch->am_in)-strtotime($official_am_in))/3600,2);
                 //     $pm_late = round((strtotime($punch->pm_in)-strtotime($official_pm_in))/3600,2);
                 //     $late = ($am_late > 0 ? $am_late : 0) + ($pm_late > 0 ? $pm_late : 0) - $ten_min_allowance;
 
-                //     $under = round((strtotime($official_am_out) - 
+                //     $under = round((strtotime($official_am_out) -
                 //         strtotime($punch->am_out))/3600,2);
-            // } 
+            // }
         $am_in = $day."_am_in";
 
         $am_out = $day."_am_out";
 
         $pm_in = $day."_pm_in";
 
-        $pm_out = $day."_pm_out";        
-       
+        $pm_out = $day."_pm_out";
+
         $official_am_in = $searched_user->shift->$am_in??false;
-        $official_am_out = $searched_user->shift->$am_out??false;          
+        $official_am_out = $searched_user->shift->$am_out??false;
         $official_pm_in = $searched_user->shift->$pm_in??false;
         $official_pm_out = $searched_user->shift->$pm_out??false;
 
-        $manual_shift_dates = collect($searched_user->manual_shifts)
-        
-        ->map(function ($manual) {              
-            
-            $start_shift = $manual->date?? 0; 
+        $manual_shift_dates = [];
 
-            $end_shift = $manual->end_shift?? 0;                         
+        foreach($searched_user->manual_shifts as $shift){
+
+            $start_shift = $shift->date ?? 0;
+
+            $end_shift = $shift->end_shift?? 0;
 
             $period = CarbonPeriod::create($start_shift, $end_shift);
-            
-            $array_of_dates = $period->toArray();        
 
-            $collection = collect($array_of_dates); 
-             
-            return $collection->map(function ($each_day) {
+            $array_of_dates = $period->toArray();
 
-                    Carbon::parse($each_day)->format('Y-m-d');
+            foreach($array_of_dates as $each){
+                $manual_shift_dates[] = Carbon::parse($each)->format('Y-m-d');
+            }
+        }
 
-            })->toArray();
+        if (in_array($date->format('Y-m-d'), $manual_shift_dates)){
+            echo $date->format('Y-m-d').'| naa na'.'<br>';
+        } else{
+            echo $date->format('Y-m-d') . '| wala pa ni' . '<br>';
+        }
 
-        })->toArray();
+        // dd($manual_shift_dates);
 
-        dd($manual_shift_dates);
-        
-        
-        
+        // collect($searched_user->manual_shifts)
 
-          
-        
+        // ->map(function ($manual) {
+
+        //     $start_shift = $manual->date?? 0;
+
+        //     $end_shift = $manual->end_shift?? 0;
+
+        //     $period = CarbonPeriod::create($start_shift, $end_shift);
+
+        //     $array_of_dates = $period->toArray();
+
+        //     collect($array_of_dates)
+
+        //     ->map(function($parseable){
+
+        //         $manual_shift_dates[] =  Carbon::parse($parseable);
+
+        //     });
+
+        // });
 
 
         if( $searched_user->manual_shifts->pluck('date')->contains( $date->format('Y-m-d')))
-        
-        {                             
+
+        {
             $shift_id =  $searched_user->manual_shifts->where('date',$date->format('Y-m-d'))
-                            ->pluck('shift_id')->implode(', ');                                                          
+                            ->pluck('shift_id')->implode(', ');
             $official_am_in     = $searched_user->shift->find($shift_id)->am_in??false;
             $official_am_out    = $searched_user->shift->find($shift_id)->am_out??false;
             $official_pm_in     = $searched_user->shift->find($shift_id)->pm_in??false;
             $official_pm_out    = $searched_user->shift->find($shift_id)->pm_out??false;
-        } 
+        }
 
         return (object) [
             'am_in'     => $official_am_in ?? false,
 
             'am_out'    => $official_am_out ?? false,
-            
+
             'pm_in'     => $official_pm_in ?? false,
 
             'pm_out'    => $official_pm_out ?? false,
 
-            'am_num_hr' => round((strtotime($official_am_out) - 
+            'am_num_hr' => round((strtotime($official_am_out) -
                             strtotime($official_am_in))/3600,2),
 
-            'pm_num_hr' => round((strtotime($official_pm_out) - 
+            'pm_num_hr' => round((strtotime($official_pm_out) -
                             strtotime($official_pm_in))/3600,2)
-            
+
         ];
- 
+
     }
 }
